152385fc378f90017913255621637fdbe5819342
Nour El Mokhtari <60050784+cmdsnr@users.noreply.github.com>
2026-02-01 16:19:04 -0500

    Update smsend.py

--- smsend.py
+++ smsend.py
@@ -36,10 +36,10 @@ def Intro():
 ⠀⠀⠈⠙⠛⠛⠛⠛⠛⠛⠛⠉⠁⠀⠀⠀⠀⠈⠉⠛⠛⠛⠛⠛⠛⠛⠋⠁⠀⠀
           {Colors.RESET}
           - SMSend -
-         {Colors.BLUE} By Sytaxus / cmdSNR {Colors.RESET}
+         {Colors.BLUE} By cmdSNR {Colors.RESET}
     Instructions: This tool may contain some {Colors.RED} limitations{Colors.RESET}.
-    - In regards to questions, open an issue in the github repo
-    - Check the https://github.com/x86senniral/SMSend-Anon-SMS-Sender/ , for full documentation if stuck.
+    - In regards to questions, open an issue in the github repo.
+    - Check the https://github.com/cmdsnr/SMSend-Anon-SMS-Sender/ , for full documentation if stuck.
 """)
     
 def cls_scrn():
